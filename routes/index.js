0f6da2f491b4edd169d4a93e59135534ac35b606
赵瑞鹏 <rpdict@gmail.com>
2016-08-24 23:17:28 +0800

    Add remove function

--- routes/index.js
+++ routes/index.js
@@ -237,6 +237,19 @@ router.post('/edit/:name/:day/:title', function (req, res) {
   });
 });
 
+router.get('/remove/:name/:day/:title', checkLogin);
+router.get('/remove/:name/:day/:title', function (req, res) {
+  var currentUser = req.session.user;
+  Post.remove(currentUser.name, req.params.day, req.params.title, function (err) {
+    if (err) {
+      req.flash('error', err);
+      return res.redirect('back');
+    }
+    req.flash('success', '删除成功!');
+    res.redirect('/');
+  });
+});
+
 function checkLogin(req, res, next) {
   if (!req.session.user) {
     req.flash('error', '未登录!');
